e89871f87325b8f9ba0327eb836cb7e74909f211
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2024-08-14 14:24:26 +0530

    update:readme

--- src/lib/components/Team.svelte
+++ src/lib/components/Team.svelte
@@ -1,33 +1,32 @@
 <script>
-    let team = [
-        {
-            name:"Nitish",
-            mail:"abc"
-        },
-        {
-            name:"Taha",
-            mail:"abc"
-        },
-        {
-            name:"Rishabh",
-            mail:"abc"
-        },
-        {
-            name:"Misbah",
-            mail:"abc"
-        }
-    ]
+	let team = [
+		{
+			name: 'Taha',
+			mail: 'abc'
+		},
+		{
+			name: 'Nitish',
+			mail: 'abc'
+		},
+		{
+			name: 'Rishabh',
+			mail: 'abc'
+		},
+		{
+			name: 'Misbah',
+			mail: 'abc'
+		}
+	];
 </script>
 
 {#each team as member}
-<div class="text-center">
-    <div class="avatar placeholder my-2">
-        <div class="bg-neutral text-neutral-content w-8 rounded-full">
-          <div>{member.name.split("")[0]}</div>
-        </div>
-    </div>
-    <br>
-    <span class="h-fit">{member.name}</span>
-</div>
+	<div class="text-center">
+		<div class="avatar placeholder my-2">
+			<div class="w-8 rounded-full bg-neutral text-neutral-content">
+				<div>{member.name.split('')[0]}</div>
+			</div>
+		</div>
+		<br />
+		<span class="h-fit">{member.name}</span>
+	</div>
 {/each}
-
